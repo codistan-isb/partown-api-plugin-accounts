c583e6c3967ee97007910c6edb28cf4a53b5e34f
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-05-05 10:25:17 +0500

    fix: fixed invite user mutation

--- src/util/inviteUserEmail.js
+++ src/util/inviteUserEmail.js
@@ -12,9 +12,21 @@ export default async function inviteUserEmail(context, recipientEmail, userId) {
   if (!shop) throw new ReactionError("not-found", "Shop not found");
 
   // let email = _.get(account, "emails[0].address");
-
+  const headerMsg = "You are invited to partOwn";
+  const bodyMsg = "PartOwn Description";
+  const website = "https://dev.partown.co/en?";
+  const emailForTemplate = "dev@partown.co";
+  const linkedIn = "https://www.linkedin.com/";
+  const logoImage = "https://i.imgur.com/xgJX3WK.jpeg";
   const dataForEmail = {
-    url: "https://dev.partown.co/en?",
+    logoImage,
+    userEmail: recipientEmail,
+    headerMsg,
+    bodyMsg,
+    website,
+    email: emailForTemplate,
+    linkedIn,
+    // url: "https://dev.partown.co/en?",
   };
 
   const language = shop.language;
