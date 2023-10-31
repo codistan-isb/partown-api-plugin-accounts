7ac6ecb3ca7f90b01b61755da45da6c0f0908043
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-10-31 11:42:04 +0500

    fix: startup function

--- src/startup/updateSuperAdminPermissions.js
+++ src/startup/updateSuperAdminPermissions.js
@@ -1,11 +1,15 @@
 export default async function updateSuperAdminPermissions(context) {
   const { Accounts } = context.collections;
 
-  const superAdminAccount = Accounts.fineOne({
+  console.log("in account startup function");
+
+  const superAdminAccount = Accounts.findOne({
     adminUIShopIds: { $ne: null, $exists: true, $not: { $size: 0 } },
   });
 
   if (!superAdminAccount) return;
+
+  console.log("super admin account is ", superAdminAccount);
 
   await Accounts.findOneAndUpdate(
     {
